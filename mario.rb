a86382461cb8a337fe1d986e3e9ac20b2477a326
Andrew Fader <andrevan@gmail.com>
2012-09-26 01:15:38 -0400

    8 way movement

--- mario.rb
+++ mario.rb
@@ -1,30 +1,52 @@
 # require 'chingu'
 class Mario < Chingu::GameObject
+  ANIMATION_SPEED = 8
+  def button_down?(*args)
+    $window.button_down?(*args)
+  end
+
   def initialize options={}
     super(options.merge(image: Gosu::Image["./images/marioD02.gif"]))
+    @counter = 0
   end
 
   def move_left
     @x -= 2
-    if @down
+    if @down || button_down?(Gosu::KbDown)
       @image = Gosu::Image["./images/marioDL02.gif"]
-    elsif @up
+    elsif @up || button_down?(Gosu::KbUp)
       @image = Gosu::Image["./images/marioUL02.gif"]
     else
       @image = Gosu::Image["./images/marioL02.gif"]
+    end
+
+    count
+    @left = true
+  end
+
+  def count
+    if @counter == ANIMATION_SPEED
+      @up = false
+      @down = false
+      @left = false
+      @right = false
+      @counter = 0
+    else
+      @counter+=1
     end
   end
 
   def move_right
     @x += 2
-    if @down
+    if @down || button_down?(Gosu::KbDown)
       @image = Gosu::Image["./images/marioDR02.gif"]
-    elsif @up
+    elsif @up || button_down?(Gosu::KbUp)
       @image = Gosu::Image["./images/marioUR02.gif"]
     else
       @image = Gosu::Image["./images/marioR02.gif"]
     end
-    @direction = 0
+    count
+    @right = true
   end
 
   def halt_left
@@ -37,21 +59,33 @@ class Mario < Chingu::GameObject
 
   def move_up
     @y -= 2
-    @image = Gosu::Image["./images/marioU02.gif"]
+    if @left || button_down?(Gosu::KbLeft)
+      @image = Gosu::Image["./images/marioUL02.gif"]
+    elsif @right || button_down?(Gosu::KbRight)
+      @image = Gosu::Image["./images/marioUR02.gif"]
+    else
+      @image = Gosu::Image["./images/marioU02.gif"]
+    end
+    count
     @up = true
   end
 
   def move_down
     @y += 2
-    @image = Gosu::Image["./images/marioD02.gif"]
+    if @left || button_down?(Gosu::KbLeft)
+      @image = Gosu::Image["./images/marioDL02.gif"]
+    elsif @right || button_down?(Gosu::KbRight)
+      @image = Gosu::Image["./images/marioDR02.gif"]
+    else
+      @image = Gosu::Image["./images/marioD02.gif"]
+    end
+    count
     @down = true
   end
 
   def halt_up
-    @up = false
   end
 
   def halt_down
-    @down = false
   end
 end
